ee4f2e4067f4282413d9df74b12a2a3375f6d904
slavara <ratdg@ya.ru>
2014-03-21 14:57:16 +0400

    Add UsePrivateExplicitly

--- HXAdvancedCodeGeneratorPlugin/Settings.cs
+++ HXAdvancedCodeGeneratorPlugin/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AdvancedCodeGenerator
@@ -7,5 +8,11 @@ namespace AdvancedCodeGenerator
     [Serializable]
     class Settings
     {
+        [Category("Haxe")]
+        [DisplayName("Use private modifier explicitly")]
+        [Description("Use private modifier explicitly for methods, vars, accessors")]
+        [DefaultValue(true)]
+        public Boolean UsePrivateExplicitly { get; set; }
+
     }
 }
